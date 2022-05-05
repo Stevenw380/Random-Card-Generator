d006e577766e368d44aea80382d959f4ebb890fe
Stevenw380 <stevenvargasw380@gmail.com>
2022-05-05 03:05:30 +0000

    card random

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.getElementById("numero").innerHTML = "7";
+  document.getElementById("pick-up").innerHTML = "♦";
+  document.getElementById("pick-down").innerHTML = "♦";
+  document.getRandomNumber;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "k"];
+  let pickUp = ["♦", "♥", "♠", "♣"];
+  let pickDown = ["♦", "♥", "♠", "♣"];
+  let indexnumero = Math.floor(Math.random() * numero);
+  return numero[indexnumero];
 };
